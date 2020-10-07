d81e9486ff77a20ac698ac04e7ef24cae248b7eb
akaimar <39114801+akaimar@users.noreply.github.com>
2020-10-07 16:50:30 +0300

    Update praktikum2.sql

--- praktikum2.sql
+++ praktikum2.sql
@@ -1,27 +1,27 @@
-/* Klubi Laudnikud liikmete nimekiri eesnimi ja perenimi tähest järjekorras 
+/* 1. Klubi Laudnikud liikmete nimekiri eesnimi ja perenimi tähest järjekorras 
 */
 SELECT eesnimi,perenimi FROM isikud KEY JOIN klubid 
 WHERE nimi ='Laudnikud' ORDER BY eesnimi,perenimi
 
-/* Klubi Laudnikud liikmete arv 
+/* 2. Klubi Laudnikud liikmete arv 
 */
 SELECT COUNT(*) FROM isikud KEY JOIN klubid
 WHERE nimi='Laudnikud'
 
-/* V tähega algavate klubide M tähega algavate eesnimedega prekonnanimed 
+/* 3. V tähega algavate klubide M tähega algavate eesnimedega prekonnanimed 
 */
 SELECT perenimi FROM isikud WHERE eesnimi LIKE 'M%' AND klubi IN (SELECT id FROM klubid WHERE nimi LIKE 'V%')
 
-/* kõige esimesena alanud partii algusaeg
+/* 4. kõige esimesena alanud partii algusaeg
 */
 SELECT algushetk FROM partiid WHERE id = 1
 
-/* Leida partiide mängijad (viited mängijatele (väljad: valge ja must)) 
+/* 5. Leida partiide mängijad (viited mängijatele (väljad: valge ja must)) 
 mis algasid 04. märtsil 2005 aastal ajavahemikus 9:00 kuni 11:00
 */
 SELECT valge, must FROM partiid WHERE algushetk BETWEEN '2005-03-04 09:00:00' AND '2005-03-04 11:00:00'
 
-/* Leida valgetega võitnute (valge_tulemus=2) isikute nimed (eesnimi, perenimi), kus partii
+/* 6. Leida valgetega võitnute (valge_tulemus=2) isikute nimed (eesnimi, perenimi), kus partii
 kestis 9 kuni 11 minutit (vt funktsiooni Datediff(); Datediff(minute, <algus>, <lõpp>)).
 */
 
@@ -30,31 +30,26 @@ JOIN partiid ON isikud.id = partiid.valge
 WHERE valge_tulemus = 2 
 AND datediff(minute, algushetk, lopphetk) BETWEEN 9 AND 11
 
-/* Leida tabelis Isikud rohkem kui 1 kord esinevad perekonnanimed (ja ei muud)
+/* 7. Leida tabelis Isikud rohkem kui 1 kord esinevad perekonnanimed (ja ei muud)
 */
 SELECT perenimi FROM isikud
 GROUP BY perenimi
 HAVING COUNT (perenimi) > 1
 
-/* Leida klubid (nimi ja liikmete arv), kus on alla 4 liikme.
+/* 8. Leida klubid (nimi ja liikmete arv), kus on alla 4 liikme.
 */
 SELECT nimi, count(*) AS liikmeid
 FROM isikud JOIN klubid ON isikud.klubi = klubid.id
 GROUP BY nimi HAVING liikmeid < 4
 
-/*  Leida kõigi Arvode poolt kokku valgetega mängitud partiide arv.
-*/
-SELECT COUNT(*) AS partiide_arv FROM partiid
-JOIN isikud ON partiid.valge = isikud.id
-
-/*  Leida kõigi Arvode poolt kokku valgetega mängitud partiide arv.
+/* 9. Leida kõigi Arvode poolt kokku valgetega mängitud partiide arv.
 */
 SELECT COUNT(*) FROM partiid
 JOIN isikud ON partiid.valge = isikud.id 
 GROUP BY eesnimi HAVING eesnimi = 'Arvo'
 
 /*
-Leida kõigi Arvode poolt kokku valgetega mängitud partiide arv turniiride lõikes
+10. Leida kõigi Arvode poolt kokku valgetega mängitud partiide arv turniiride lõikes
 (turniiri id ja partiide arv).
 */
 SELECT turniirid.Id, count(*)
@@ -63,7 +58,7 @@ ON partiid.turniir = turniirid.id JOIN isikud
 ON isikud.id = partiid.valge WHERE eesnimi = 'Arvo' GROUP BY turniirid.Id  ORDER BY turniirid.Id
 
 /*
-Leida kõigi Mariade poolt kokku mustadega mängitud partiidest saadud punktide arv
+11. Leida kõigi Mariade poolt kokku mustadega mängitud partiidest saadud punktide arv
 (tulemus = 2 on võit ja annab ühe punkti, tulemus = 1 on viik ja annab pool punkti)
 */
 
@@ -75,8 +70,7 @@ SELECT SUM(musta_tulemus)/2 FROM partiid
 JOIN isikud ON isikud.id = partiid.must
 WHERE eesnimi = 'Maria'
 
-/*
-Leida partiide keskmine kestvus turniiride kaupa (tulemuseks on tabel 2 veeruga:
+/* 12. Leida partiide keskmine kestvus turniiride kaupa (tulemuseks on tabel 2 veeruga:
 turniiri nimi, keskmine partii pikkus).
 */
 
