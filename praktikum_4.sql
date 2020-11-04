a46166104fd390a34aec22b60c3c755a48088412
akaimar <39114801+akaimar@users.noreply.github.com>
2020-11-04 15:49:07 +0200

    Update praktikum_4.sql

--- praktikum_4.sql
+++ praktikum_4.sql
@@ -41,3 +41,19 @@ END
 
 SELECT f_mangija_koormus(80)
 
+/* Funktsioon ühe mängija võitude arv turniiril f_mangija_voite_turniiril(isikud.id, turniirid.id)
+*/
+
+ALTER FUNCTION f_mangija_voite_turniiril(isikud_id INTEGER, turniirid_id INTEGER)
+    RETURNS INTEGER
+BEGIN
+    DECLARE tulemus INTEGER;
+        SELECT COUNT(*) INTO tulemus
+        FROM v_punktid
+        WHERE mangija = isikud_id AND punkt > 0 AND turniir = turniirid_id;
+    RETURN tulemus;
+
+END 
+
+SELECT f_mangija_voite_turniiril(71,41)
+
