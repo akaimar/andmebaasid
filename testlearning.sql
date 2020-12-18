2fe6ce74cbe8d27a9129f4c8ddcef1af06df3f8a
akaimar <39114801+akaimar@users.noreply.github.com>
2020-12-18 15:24:42 +0200

    Create testlearning.sql

---
+++ testlearning.sql
@@ -0,0 +1,28 @@
+
+
+/*protseduur*/
+
+create procedure testime @id integer
+AS
+SELECT * FROM TURNIIRID WHERE id = @id
+GO;
+
+exec testime @id = 41
+
+/*funktsioon*/
+CREATE FUNCTION teemidagi(sisend1 integer) 
+RETURNS VARCHAR(100)
+NOT DETERMINISTIC 
+BEGIN
+    DECLARE väljund VARCHAR(100);
+    SELECT nimi INTO väljund FROM klubid WHERE id = sisend1;
+    RETURN väljund;
+END;
+
+
+/*group by having - kus tabelis tuleb mängijad, kes on võitnud ainult ühe mängu*/
+SELECT must, count(*) FROM partiid 
+WHERE musta_tulemus = 2
+GROUP BY must
+HAVING count(musta_tulemus) = 1
+
